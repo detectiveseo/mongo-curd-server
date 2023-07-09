6d9894a4474ae8ee9f109f309556035367d8cb60
detectiveseo <detectiveseo1@gmail.com>
2023-07-09 10:29:36 +0600

    added queary for paginaiton

--- deletes.js
+++ deletes.js
@@ -1,6 +1,12 @@
+const { ObjectId } = require("mongodb");
+
 module.exports = deletes = (app, InfoCollection) => {
     app.delete("/remove/:id", async(req, res) => {
         const id = req.params.id;
-        console.log(id);
+        const query = {_id: new ObjectId(id)}
+        const result = await InfoCollection.findOneAndDelete(query);
+        console.log(result);
+        res.send(result);
+
     })
 }
--- get.js
+++ get.js
@@ -1,7 +1,15 @@
 module.exports = get = (app, InfoCollection) => {
     app.get("/users", async (req, res) => {
-        const result = await InfoCollection.find().toArray();
-        console.log( "HITTED")
+        console.log(req.query)
+        const page = parseInt(req.query.page) || 1;
+        const itemsLimit = parseInt(req.query.items) || 6;
+        const skip = (page - 1) * itemsLimit;
+        const result = await InfoCollection.find().skip(skip).limit(itemsLimit).toArray();
         res.send(result);
+    })
+
+    app.get("/total-items", async(req, res) => {
+        const result = await InfoCollection.estimatedDocumentCount();
+        res.send({totalItems: result});
     })
 }
